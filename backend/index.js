6ebe115c4231aa05c1d7aa809887c31da7439cf1
Phan Minh Quoc <120693506+QuocPhan9@users.noreply.github.com>
2025-03-23 22:48:56 +0700

    deploy

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,12 @@ connectMongoDB();
 
 app.use(helmet());
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://social-media-vert-eta.vercel.app", // Cho phép frontend truy cập
+        credentials: true,
+    })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
