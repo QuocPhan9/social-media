2afde010f3bdae222a966514221ca0b6deddb386
Phan Minh Quoc <120693506+QuocPhan9@users.noreply.github.com>
2025-03-25 23:34:41 +0700

    add user/pass

--- frontend/src/auth/LoginPage/Login.jsx
+++ frontend/src/auth/LoginPage/Login.jsx
@@ -57,7 +57,7 @@ const Login = () => {
     }
 
     return (
-        <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
+        <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-2'>
             <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
                 {/* LEFT */}
                 <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
@@ -120,12 +120,16 @@ const Login = () => {
                             (
                                 <CustomButton 
                                     type="submit"
-                                    containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+                                    containerStyles={`inline-flex justify-center rounded-md bg-blue px-6 py-3 text-sm font-medium text-white outline-none`}
                                     title='Login'
                                 />
                                 
                         )}
                     </form>
+                    <div className='text-ascent-2 text-sm text-center'>
+                    <h6>Note: You can use this existing account</h6>
+                    <p>pmq28434@gmail.com | 123456</p>
+                    </div>
                     <p className='text-ascent-2 text-sm text-center'>
                         Don't have an account?
                         <Link
